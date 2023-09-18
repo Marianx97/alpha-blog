5aebfde7ebc608f5750f7e3d91b68f24b9013b6d
Mariano Ranucci <ranucci.mariano@gmail.com>
2023-09-18 17:57:27 -0300

    Added destroy  action to ArticlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,4 +41,10 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end

d1f742da52352cc6c035ab6cd96ea06d54ad18d1
Mariano Ranucci <ranucci.mariano@gmail.com>
2024-01-15 20:42:51 -0300

    Added view level authorization

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(permited_params)
-    @article.user = User.first
+    @article.user = current_user
 
     if @article.save
       flash[:notice] = 'Article was created successfully.'
